b85826dd26b9925ce7267c5b2b5307819d3c3e53
Ben Gates <benjamin_gates@outlook.com>
2021-09-03 22:02:35 -0600

    cleaned up formatting for notifications page

--- src/notifications/Notifications.js
+++ src/notifications/Notifications.js
@@ -1,12 +1,45 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import notifications from "../data/notifications.json";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Notifications() {
+  const notificationsList = notifications.data;
+  const notificationsElement = notificationsList.map(
+    ({ title, message, image_url, date }, index) => {
+      return (
+        <Card key={index} style={{marginBottom: "10px"}}>
+          <Card.Header>{date}</Card.Header>
+          <Card.Body>
+            <Row>
+              <Col xs={10}>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{message}</Card.Text>
+              </Col>
+
+              {image_url ? (
+                <Col xs={2}>
+                  <Card.Img
+                    src={`https://i.${image_url.slice(8)}.jpg`}
+                    title="source: imgur.com"
+                    alt={"mantis image"}
+                    style={{ width: "100%" }}
+                  />
+                </Col>
+              ) : null}
+            </Row>
+          </Card.Body>
+        </Card>
+      );
+    }
+  );
   return (
-    <Container fluid as="main">
+    <Container fluid as="main" style={{marginBottom: "40px"}}>
       <h1>Notifications</h1>
       <hr />
-      <div>Hello World!</div>
+      {notificationsElement}
     </Container>
   );
 }
