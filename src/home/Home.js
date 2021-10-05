d2f815f9e6bd2d1dc1dc55a4d5a897ec95d7c5c3
Ben Gates <benjamin_gates@outlook.com>
2021-10-05 16:42:20 -0600

    added ajax for carousel images on home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
 import galinthiasamoena2 from "./img/galinthias-amoena-2.jpg";
 import majangellamoultoni from "./img/majangella-moultoni.jpg";
+import {listImages} from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function Home() {
-  const imageURLs = [
+  const [images, setImages] = useState([]);
+  const [imagesError, setImagesError] = useState(null);
+  useEffect(loadImages, []);
+  function loadImages() {
+    const abortController = new AbortController();
+    setImagesError(null);
+    listImages(abortController.signal).then(setImages).catch(setImagesError);
+    return () => {
+      abortController.abort();
+    };
+  }
+
+  /*const imageURLs = [
     'https://imgur.com/HUU7CWt',
     'https://imgur.com/do3JW7f',
     'https://imgur.com/jx9sWmH',
     'https://imgur.com/A0wQ8qv',
-  ];
+  ];*/
 
-  const imageElements = imageURLs.map((url, index) => {
+  const carouselImages = images.filter(({carousel_image}) => carousel_image)
+
+  const imageElements = carouselImages.map(({image_url}, index) => {
     return (
       <Carousel.Item style={{ display: "flex", justifyContent: "center" }}>
-        <img src={`https://i.${url.slice(8)}.jpg`} title="source: imgur.com" alt={"image"+index} height="550px" />
+        <img src={`https://i.${image_url.slice(8)}.jpg`} title="source: imgur.com" alt={"image"+index} height="550px" />
         <Carousel.Caption>
           <h5 style={{ color: "#C3C9E3" }}>Image {index}</h5>
         </Carousel.Caption>
@@ -28,9 +44,10 @@ function Home() {
       as="main"
       style={{ backgroundColor: "black", margin: "0px", padding: "20px" }}
     >
-      <Carousel fade>
+      {images.length ? <Carousel fade>
        {imageElements}
-      </Carousel>
+       <ErrorAlert error={imagesError} />
+      </Carousel> : <div>Loading...</div>}
     </Container>
   );
 }
